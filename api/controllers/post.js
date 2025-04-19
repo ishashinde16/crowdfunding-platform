815d602c2f6664b3381f4c4c4a6cad8138bab197
ishashinde <isha16shinde@gmail.com>
2025-04-19 15:31:21 +0530

    updates to home page display and styling

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,16 @@
-export const addPost= (req,res)=>{
-    res.json("from controller")
-}
+import { db } from "../db.js";
+
+export const getCampaigns = (req, res) => {
+  const category = req.query.cat;
+
+  let q = "SELECT * FROM campaign"; 
+
+  if (category) {
+    q = "SELECT * FROM campaign WHERE category = ?";
+  }
+
+  db.query(q, category ? [category] : [], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json(data);
+  });
+};
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,40 +1,34 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import Logo from "../images/logo.png"
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import Logo from "../images/logo.png";
+
 const Navbar = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const activeCategory = searchParams.get('cat'); // example: 'medical', 'arts' etc.
+
   return (
     <div className='navbar'>
       <div className='container'>
         <div className='logo'>
-          <img src={Logo } alt="CrowdX" />
+          <img src={Logo} alt="CrowdX" />
         </div>
         <div className='links'>
-          <Link className='link' to="/?cat=medical">
-            <h6>MEDICAL</h6>
+          {['medical', 'business', 'arts', 'education', 'social'].map((cat) => (
+            <Link className={`link ${activeCategory === cat ? 'active' : ''}`} key={cat} to={`/?cat=${cat}`}>
+              <h6>{cat.toUpperCase()}</h6>
             </Link>
-            <Link className='link' to="/?cat=business">
-            <h6>BUSINESS</h6>
-            </Link>
-            <Link className='link' to="/?cat=arts">
-            <h6>ARTS</h6>
-            </Link>
-            <Link className='link' to="/?cat=education">
-            <h6>EDUCATION</h6>
-            </Link>
-            <Link className='link' to="/?cat=social">
-            <h6>SOCIAL</h6>
-            </Link>
-            <span>John</span>
-            <span>Logout</span>
-            <span className='campaign'>
-              <Link className="link" to="/campaign">Launch</Link>
-            </span>
-        
+          ))}
+
+          <span>John</span>
+          <span>Logout</span>
+          <span className='campaign'>
+            <Link className="link" to="/campaign">Launch</Link>
+          </span>
         </div>
       </div>
-    
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
